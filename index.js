e7a5f24a7315a1641285780986f1892d7f69db81
aman <amankun77@gmail.com>
2023-07-19 18:46:02 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('.hamburger');
+const navlinks = document.querySelector('.nav-links');
+hamburger.addEventListener('click',()=>{
+    navlinks.classList.toggle('open');
+});
